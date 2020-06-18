7731d521a40f975d7d359e3b3362a8ccfda0febe
Rishab Kumar <rishabkumar7@gmail.com>
2020-06-18 16:44:22 -0400

    Added project mockups

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'I try to create noice web experiences. I am experienced in Amazon Web Services and can help clients with several related technologies.',
+  paragraphTwo: 'A young technical guy with experience in creating solutions for customers.',
+  paragraphThree: 'Dynamic and goal-oriented. Confident and poised in interactions with individuals at all levels. Focused and dedicated; I have a good eye for detail and put in my best effort in time management and prioritization.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -37,7 +37,7 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: '02.png',
+    img: '05.jpg',
     title: 'Weather Dashboard',
     info: '',
     info2: '',
@@ -46,12 +46,30 @@ export const projectsData = [
   },
   {
     id: uuidv1(),
-    img: '03.png',
+    img: '04.jpg',
     title: 'My Blog Site',
     info: '',
     info2: '',
     url: 'https://blog.rishabkumar.ga',
     repo: 'https://github.com/rishabkumar7/myblog', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: '06.jpg',
+    title: 'Cloud Notes',
+    info: '',
+    info2: '',
+    url: 'https://rishabkumar7.github.io/CloudNotes/',
+    repo: 'https://github.com/rishabkumar7/CloudNotes', // if no repo, the button will not show up
+  },
+  {
+    id: uuidv1(),
+    img: '07.jpg',
+    title: 'Hacker News Clone',
+    info: '',
+    info2: '',
+    url: 'https://hackernews-clone.rishabkumar.ga/#/',
+    repo: 'https://github.com/rishabkumar7/hackernews', // if no repo, the button will not show up
   },
 ];
 
@@ -68,27 +86,27 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/rishabk7',
     },
     {
       id: uuidv1(),
-      name: 'codepen',
+      name: 'dev',
       url: '',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/rishabkumar7/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/rishabkumar7',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
