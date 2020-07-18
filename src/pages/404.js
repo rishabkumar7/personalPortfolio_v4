42d5f402ce9e7026e770983e9bec31397fda5ca0
Rishab Kumar <rishabkumar7@gmail.com>
2020-07-18 00:24:07 -0400

    added twitter card data

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,7 @@ import SEO from "../components/SEO"
 const Error = () => {
   return (
   <Layout>  
+    <SEO title="404" description="Looks like you are lost" />
    <main className="error-page">
     <div className="error-container">
       <h1> 😔 Oops, looks like you are lost!</h1>
