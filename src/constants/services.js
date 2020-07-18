8c03a050a4a6227f0b64da47b8265e243c536cf1
Rishab Kumar <rishabkumar7@gmail.com>
2020-07-18 01:22:59 -0400

    Added Skills

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "Web Development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I'm fulent in HTML/CSS and JavaScript, also familiar with various frameworks and libraries: React, Vue, etc `,
   },
   {
     id: 2,
     icon: <FaAws className="service-icon" />,
     title: "AWS",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I have 2 years of experience with AWS backed by some certifications: Developer Associate and Solutions Architect Associate`,
   },
   {
     id: 3,
     icon: <FaLinux className="service-icon" />,
     title: "Linux",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I'm a Linux enthusist and love to create cool projects with Raspberry Pi!`,
   },
 ]
