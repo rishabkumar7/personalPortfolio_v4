a90375289b1ed9c2c3f3dad951b7c6bfd8bbfee2
Rishab Kumar <rishabkumar7@gmail.com>
2020-09-06 21:17:33 -0400

    changed the entire layout theme

---
+++ src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Rishab Kumar',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'John Doe',
+  url: 'https://rishab.cloud',
+  legalName: 'Rishab Kumar',
+  defaultDescription: 'I’m Rishab and I’m a Cloud engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/rishabk7',
+    github: 'https://github.com/rishabkumar7',
+    linkedin: 'https://www.linkedin.com/in/rishabkumar7/',
+    instagram: 'https://instagram.com/smakosh19',
+    youtube: 'https://www.youtube.com/c/rishabkumar7',
+    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: '07rishabk',
+    twitter: '@rishabk7',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'email',
+    phone: 'phone number',
+  },
+  foundingDate: '2020',
+};
